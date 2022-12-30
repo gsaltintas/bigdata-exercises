ef3cd6386bcf3d83d8950eb9de010feaa330f2b8
Gül Sena A <galtintas17@ku.edu.tr>
2022-12-30 12:09:16 +0000

    Added exercise 13

--- Big_Data/exercise07/quiz/mapreduce_q3/src/MapReduceQuery.java
+++ Big_Data/exercise07/quiz/mapreduce_q3/src/MapReduceQuery.java
@@ -12,37 +12,44 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 public class MapReduceQuery {
 
-    private static class QueryMapper extends Mapper<Object, Text, Text, IntWritable>{
+    private static class QueryMapper extends Mapper<Object, Text, Text, IntWritable> {
+
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
         private String strWord = "";
-        
+
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                strWord = itr.nextToken();
-                if (strWord.length() == 12 ) {
-                    word.set("k");
+            // counting function:
+            StringTokenizer it = new StringTokenizer(value.toString());
+            while (it.hasMoreTokens()) {
+                strWord = it.nextToken();
+                if (strWord.length() == 12) {
+                    word.set(strWord);
                     context.write(word, one);
                 }
             }
         }
     }
 
+    static int count = 0;
 
-    private static class QueryReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private static class QueryReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
         private IntWritable result = new IntWritable();
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
-            result.set(sum);
+            count += sum;
+        }
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            Text key = new Text("result");
+            result.set(count);
             context.write(key, result);
         }
     }
@@ -55,18 +62,19 @@ public class MapReduceQuery {
             System.exit(2);
         }
         // Setup a MapReduce job
-        Job job = new Job(conf, "query");
+        Job job = new Job(conf, "word count");
         job.setJarByClass(MapReduceQuery.class);
         job.setMapperClass(QueryMapper.class);
-        
-	// Can you reducer be used as a combiner? If so, enable it.
-	// You can also experiment with and without combiner and compare the running time.
-        job.setCombinerClass(QueryReducer.class);
+
+        // Can you reducer be used as a combiner? If so, enable it.
+        // You can also experiment with and without combiner and compare the running
+        // time.
+        // job.setCombinerClass(QueryReducer.class);
         job.setReducerClass(QueryReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-	// at least one reducer, experiment with this parameter
-        //job.setNumReduceTasks(8);    
+        // at least one reducer, experiment with this parameter
+        // job.setNumReduceTasks(8);
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
